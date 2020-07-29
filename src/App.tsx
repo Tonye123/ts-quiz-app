4be7f4a1cfc0b43642fb43106aba02a4ae8ea080
Tonye O <tonyeo@Tonyes-MacBook-Air.local>
2020-07-28 20:13:51 -0600

    initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { QuestionCard } from "./components/QuestionCard";
+import { fetchQuizQuestions, Difficulty } from "./API";
 
+const TOTAL_QUESTION = 10;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
+	const startTrivia = async () => {};
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+	const nextQuestion = () => {};
+	console.log(fetchQuizQuestions(TOTAL_QUESTION, Difficulty.EASY));
+
+	return (
+		<div className="App">
+			<h1>REACT QUIZ</h1>
+			<button className="start" onClick={startTrivia}>
+				start
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading Question...</p>
+			{/* <QuestionCard
+				questionNr={number + 1}
+				totalQuestions={TOTAL_QUESTION}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				callback={checkAnswer}
+			/> */}
+			<button className="next" onClick={nextQuestion}>
+				Next Question
+			</button>
+		</div>
+	);
 }
 
 export default App;
